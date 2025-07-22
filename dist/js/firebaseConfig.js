1d6167b78edae9a8b0230222ed188bd129f6d219
reyanshafi <reyannshafi@gmail.com>
2025-07-22 07:29:32 +0530

    added firebaseConfig

---
+++ dist/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfsDCX0LnmbnhduUpa_OzfeXapfcyRJng",
+  authDomain: "bus-management-system-44771.firebaseapp.com",
+  projectId: "bus-management-system-44771",
+  storageBucket: "bus-management-system-44771.appspot.com",
+  messagingSenderId: "245676169039",
+  appId: "1:245676169039:web:902e94eca32d2ec762973a",
+  measurementId: "G-SBBM9FC0Z0"
+}; 
